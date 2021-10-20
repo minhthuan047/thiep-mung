decb3d272b729eddc3ed289df4bbcdf4259013df
minhthuan047 <86427940+minhthuan047@users.noreply.github.com>
2021-10-20 21:37:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
+const nameGirl = 'ê mày';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Thú Dị Ghê Hong';
+const titleCard = 'Tặng tml';
+const contentCard = 'Cái này người ta tạo chúc mừng 20/10, tao sài ké thôi';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
